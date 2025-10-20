31c97ec70e874a1fc5eaf66f3ce7f36dacdaa1f5
Niraj Shah <nirajshah950@gmail.com>
2025-10-20 22:25:53 +0530

    utility function to connect to the database.

---
+++ src/lib/mongodb.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = "mongodb://192.168.30.106:27017/todo-next";
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectDB() {
+  if (cached.conn && mongoose.connection.readyState === 1) {
+    return cached.conn; // already connected
+  }
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        serverSelectionTimeoutMS: 30000, // increased timeout for slower connections
+        bufferCommands: false, // optional: disables buffer commands
+      })
+      .then((mongooseInstance) => {
+        console.log("MongoDB connected");
+        return mongooseInstance;
+      })
+      .catch((error) => {
+        console.error("MongoDB connection error:", error);
+        cached.promise = null; // reset promise on failure
+        throw error;
+      });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}

420cbf74b9911aee78ad3f3452229c8741ae88be
Niraj Shah <nirajshah950@gmail.com>
2025-10-21 22:43:03 +0530

    Properly define Users schema and Address schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,36 +1,91 @@
 import { Schema, models, model } from "mongoose";
 
 const UserSchema = new Schema({
-    userName:{
-        type: String,
-        required: [true,"user name is requried."]
+  userName: {
+    type: String,
+    required: [true, "Username is required."],
+  },
+  firstName: {
+    type: String,
+    required: [true, "User's first name is required."],
+  },
+  lastName: {
+    type: String,
+    required: [true, "User's last name is required."],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required."],
+    validate: {
+      validator: function (v) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid email!`,
     },
-    firstName:{
-
+  },
+  phone: {
+    type: String,
+    required: [true, "Phone number is required."],
+    validate: {
+      validator: function (v) {
+        return /^[6-9]\d{9}$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
     },
-    middleName:{
-
+  },
+  address: {
+    type: Schema.ObjectId,
+    ref: "Address",
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required."],
+    validate: {
+      validator: function (v) {
+        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9])(.{6,})$/.test(
+          v
+        );
+      },
+      message: (props) =>
+        "Password must be at least 6 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.",
     },
-    lastName:{
+  },
+});
 
-    },
-    email:{
+export const User = models.User || model("User", UserSchema);
 
-    },
-    phone:{
-
-    },
-    address:{
-
-    }
-})
 
 const AddressSchema = new Schema({
-    houseNo:{
+  houseNo: {
+    type: String,
+    required: [true, "House No is required."],
+  },
+  street: {
+    type: String,
+    required: [true, "Street is required."],
+  },
+  addressLine: {
+    type: String,
+    required: [true, "Address-Line is required."],
+  },
+  city: {
+    type: String,
+    required: [true, "City is required."],
+  },
+  state: {
+    type: String,
+    required: [true, "State is required."],
+  },
+  country: {
+    type: String,
+    required: [true, "Country is required."],
+  },
+  ZipCode: {
+    type: Number,
+    min: [99999, "Should be greter then 99999."],
+    max: [10000000, "Should be smaller then 10000000"],
+    required: [true, "Zip-Code is required."],
+  },
+});
 
-    },
-    street:{
-
-    },
-    
-})
+export const Address = models.Address || model("Address", AddressSchema);

a2dd6e7876e311c02f629203841b14bd3841b0a0
Niraj Shah <nirajshah950@gmail.com>
2025-10-22 21:44:15 +0530

    defined threads service functions.

---
+++ src/service/thread.service.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export async function PostThread(){
+
+}
+
+export async function GetPosts(){
+
+}
+
+export async function MyPosts(){
+
+}
+
+export async function DeletePost(){
+
+}
+
+export async function Search() {
+    
+}

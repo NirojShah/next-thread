b9e83295ba1d1442d82d91e243ec71b665015300
Niraj Shah <nirajshah950@gmail.com>
2025-10-24 22:19:43 +0530

    Update the thread model.

--- src/models/thread.model.js
+++ src/models/thread.model.js
@@ -1,30 +1,35 @@
 import { models, model, Schema } from "mongoose";
 
 const ThreadSchema = new Schema({
-    uploadedBy:{
-        type: Schema.ObjectId,
-        requied: [true, "Please mention the _id of user."],
-        ref: "User"
-    },
-    plainText:{
-        type: String,
-    },
-    html:{
-        type: String
-    }
-})
+  uploadedBy: {
+    type: Schema.Types.ObjectId,
+    required: [true, "Please mention the _id of user."],
+    ref: "User"
+  },
+  plainText: {
+    type: String
+  },
+  html: {
+    type: String
+  }
+});
 
-const ThreadContent = new Schema({
-    data:{
-        type : String,
-        required: [true, "Proved the file Base64 data"]
-    },
-    contentType:{
-        type: String,
-        required: [true, "ContentType is required."]
-    },
-    fileName:{
-        type: String,
-        required:[true, "FileName is required."]
-    }
-})
+const ThreadContentSchema = new Schema({
+  data: {
+    type: String,
+    required: [true, "Provide the file Base64 data"]
+  },
+  contentType: {
+    type: String,
+    required: [true, "ContentType is required."]
+  },
+  fileName: {
+    type: String,
+    required: [true, "FileName is required."]
+  }
+});
+
+const Thread = models.Thread || model("Thread", ThreadSchema);
+const ThreadContent = models.ThreadContent || model("ThreadContent", ThreadContentSchema);
+
+export { Thread, ThreadContent };

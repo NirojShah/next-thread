1e428f36c2823f275d0ce5165ad226fdfdf4509d
Niraj Shah <nirajshah950@gmail.com>
2025-11-01 15:47:13 +0530

    Updated the user creation service function.

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -2,8 +2,6 @@ import { connectDB } from "@/lib/mongodb";
 import { User, Address } from "../models/user.model";
 import { ApiError } from "next/dist/server/api-utils";
 
-
-
 export async function CreateUser({
   userName,
   firstName,
@@ -12,52 +10,44 @@ export async function CreateUser({
   email,
   phone,
   address,
-}
-// ,session
-) {
+}) {
   try {
     const isPresent = await User.findOne({
       $or: [{ userName }, { phone }, { email }],
-    })
-    // .session(session);
+    });
     if (isPresent) {
       throw new ApiError(500, "User already present.");
     }
+    const newAddress = await new Address({
+      houseNo: address.houseNo,
+      street: address.street,
+      addressLine: address.addressLine,
+      city: address.city,
+      state: address.state,
+      country: address.country,
+      ZipCode: address.ZipCode,
+    });
+    let newUser;
+    if (newAddress) {
+      newUser = await new User({
+        userName,
+        firstName,
+        lastName,
+        password,
+        email,
+        phone,
+        address: newAddress._id,
+      });
+      await newUser.save();
+    }
 
-    const newAddress = await Address.create(
-      [
-        {
-          houseNo: address.houseNo,
-          street: address.street,
-          addressLine: address.addressLine,
-          city: address.city,
-          state: address.state,
-          country: address.country,
-          ZipCode: address.ZipCode,
-        },
-      ],
-      // { session }
-    );
+    if (newUser) {
+      await newAddress.save();
+    }
 
-    const newUser = await User.create(
-      [
-        {
-          userName,
-          firstName,
-          lastName,
-          password,
-          email,
-          phone,
-          address: newAddress[0]._id,
-        },
-      ],
-      // { session }
-    );
-    console.log(newUser)
-    return newUser[0];
-
-
+    return newUser;
   } catch (error) {
     throw error;
   }
 }
+

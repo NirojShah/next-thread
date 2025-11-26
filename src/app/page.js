a049b40a8e74b83a49be655136b4d212756ebfa2
Niraj Shah <nirajshah950@gmail.com>
2025-11-26 22:53:06 +0530

    Calling the user profile page in the home page.

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import { redirect } from "next/navigation";
 import VertNav from "./Components/VertNav";
 import TopNav from "./Components/Navs/TopNav";
 import Threads from "./page/threads/page";
+import ProfilePage from "./Components/User Profile/UserProfile";
 const page = async() => {
   const session = await getServerSession(authOptions);
   if(!session){
@@ -15,7 +16,9 @@ const page = async() => {
     <div className="flex flex-col">
       <TopNav/>
       <div className="mt-[60px] mx-[16px] flex ">
-        <div className="w-[15%] border border-red-500 h-[calc(100vh-60px)]">test1</div>
+        <div className="w-[15%] border border-red-500 h-[calc(100vh-60px)]">
+          <ProfilePage/>
+        </div>
         <div className="w-[70%] border border-blue-600 h-[calc(100vh-60px)] overflow-x-scroll">
           <Threads/>
         </div>
